4d8dc5130ef75dc53a3507ba31f05b28485ce8e2
wiltruong <36658093+wiltruong@users.noreply.github.com>
2020-08-31 19:39:50 -0400

    Update mode-switcher.js

--- js/mode-switcher.js
+++ js/mode-switcher.js
@@ -1,30 +1,30 @@
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 }
 
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 } else {
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+   	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+    document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
     sessionStorage.setItem('theme', '');
   }
 }
@@ -36,28 +36,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	document.getElementById("theme-toggle").innerHTML = "<i class="fas fa-adjust"></i>";
 }
